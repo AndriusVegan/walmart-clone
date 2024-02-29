06238ed0a7ae96ad0c14d7ab0e1ee019a26d64aa
AndriusVegan <lifecoachandy7@gmail.com>
2024-02-29 17:04:25 +0200

    Fix groupBySKU bug, just need to sort close the shopping cart functionality

--- lib/groupBySKU.ts
+++ lib/groupBySKU.ts
@@ -1,13 +1,16 @@
 import { Product } from "@/typings/productTypings";
 
 export function groupBySKU(products: Product[]): Record<string, Product[]> {
-  return products?.reduce((accumulator: Record<string, Product[]>, currentProduct: Product[]) => {
-    const sku = currentProduct.meta.sku;
-    if (!accumulator[sku]) {
-        // if product does not exist, create empty array 
+  return products?.reduce(
+    (accumulator: Record<string, Product[]>, currentProduct: Product) => {
+      const sku = currentProduct.meta.sku;
+      if (!accumulator[sku]) {
+        // if product does not exist, create empty array
         accumulator[sku] = [];
-    }
-    accumulator[sku].push(currentProduct);
-    return accumulator
-  }, {});
+      }
+      accumulator[sku].push(currentProduct);
+      return accumulator;
+    },
+    {}
+  );
 }
